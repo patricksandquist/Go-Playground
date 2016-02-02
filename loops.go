50347ba51704e5cd72b2bcff21af0fc78ebb19cc
Patrick Sandquist <patricksandquist@gmail.com>
2016-02-02 14:14:30 -0800

    Add switches

--- loops.go
+++ loops.go
@@ -43,4 +43,21 @@ func main() {
   } else {
     fmt.Println(num, " has multiple digits")
   }
+  
+  // Switches are pretty similar
+  switch time.Now().Weekday() {
+    case time.Saturday, time.Sunday:
+      fmt.Println("it's the weekend")
+    default:
+      fmt.Println("it's a weekday")
+  }
+  
+  // But may also be written in some different ways:
+  t := time.Now()
+  switch {
+    case t.Hour() < 12:
+      fmt.Println("it's before noon")
+    default:
+      fmt.Println("it's after noon")
+  }
 }
