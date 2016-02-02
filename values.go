5fc5ddcb763bd157ffa327206068ba114583408b
Patrick Sandquist <patricksandquist@gmail.com>
2016-02-02 13:42:38 -0800

    Make values

---
+++ values.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+  // Concatinating strings like a boss
+  fmt.Println("Patrick" + " " + "Sandquist" + " knows Go!")
+  
+  // Arithmetic
+  fmt.Println("1 + 1 =", 1 + 1)
+  fmt.Println("7.0 / 3.0 =", 7.0 / 3.0)
+  fmt.Println("7 / 3 =", 7 / 3)
+  
+  // Booleans and whatnot
+  fmt.Println(true && false)
+  fmt.Println(true || false)
+  fmt.Println(!true)
+}
