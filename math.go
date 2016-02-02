de80d6d2906fc54ebbfe9e6cf2ad6a8d99d8ff12
Patrick Sandquist <patricksandquist@gmail.com>
2016-02-02 13:59:24 -0800

    Add some math to math.go

--- math.go
+++ math.go
@@ -7,8 +7,17 @@
 package main
 
 import "fmt"
-import "math"
+import m "math" // m is the alias for the math package
+
+// This is a constant
+const s string = "immutable"
 
 func main() {
+  // This is a numerical constant. It has no type until explicitely stated/cast
+  const n = 1000
+  const d = 3e20 / n
   
+  // Here we cast to force a type on the constant
+  fmt.Println(int64(d))
+  fmt.Println(m.Sin(n))
 }
