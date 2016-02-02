fd5f497755ea50ca86086898cacd9f70fee31191
Patrick Sandquist <patricksandquist@gmail.com>
2016-02-02 13:46:52 -0800

    Add more

--- values.go
+++ values.go
@@ -15,4 +15,23 @@ func main() {
   fmt.Println(true && false)
   fmt.Println(true || false)
   fmt.Println(!true)
+  
+  // Declaring some variables and their types
+  var a string = "testing!"
+  
+  // Two at a time!
+  var b, c int = 3, 4
+  fmt.Println(a)
+  fmt.Println(b + c)
+  
+  // Can be lazy and leave off type
+  var hungry = true
+  
+  // Or can leave off value
+  var e int
+  fmt.Println(e) // -> 0
+  
+  // But let's stick to shorthand:
+  f := "speedy"
+  fmt.Println(f)
 }
