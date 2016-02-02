1b78ff94e28a0cdfbb911654bcf4460efc494c55
Patrick Sandquist <patricksandquist@gmail.com>
2016-02-02 13:36:50 -0800

    Create hello world

---
+++ hello-world.go
@@ -0,0 +1,10 @@
+// Main package for starting document
+package main
+
+// Import standard library
+import "fmt"
+
+// This is the main executable function
+func main() {
+  fmt.Println("hello world")
+}
